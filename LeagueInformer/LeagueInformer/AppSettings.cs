83c28dc80240007a5b192672eb83f20b7b3ff503
Bartosz Mroz <bartosz.mroz@ecom.software>
2018-10-27 00:07:25 +0200

    Podmiana klucza API na stały przydzielony przed Riot Games

--- LeagueInformer/LeagueInformer/AppSettings.cs
+++ LeagueInformer/LeagueInformer/AppSettings.cs
@@ -3,6 +3,6 @@
     public class AppSettings
     {
         public const string CheckInternetConnectionString = "http://clients3.google.com/generate_204";
-        public const string AuthorizationApiKey = "RGAPI-5cd891aa-8288-469d-ab79-176c6de305a0";
+        public const string AuthorizationApiKey = "RGAPI-2958ec04-0412-63bf-1ddc-2aba3fe0bfbe";
     }
 }
