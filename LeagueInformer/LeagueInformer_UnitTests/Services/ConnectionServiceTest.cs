c223e7969d686b84af1afbd88b93c30eb4faee79
Bartosz Mroz <bartosz.mroz@ecom.software>
2018-10-27 20:20:54 +0200

    Dodanie projektu UnitTestów oraz NuGet Moq

---
+++ LeagueInformer/LeagueInformer_UnitTests/Services/ConnectionServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using LeagueInformer.Interfaces;
+using LeagueInformer.Services;
+using Moq;
+using Xunit;
+
+namespace LeagueInformer_UnitTests.Services
+{
+    public class ConnectionServiceTest
+    {
+        private readonly ConnectionService _connection;
+
+        public ConnectionServiceTest()
+        {
+            _connection = new ConnectionService();
+        }
+        [Fact]
+        public void ConnectionService_RequestReturnedSuccess()
+        {
+            Assert.True(_connection.HasInternetConnection());
+        }
+    }
+}
