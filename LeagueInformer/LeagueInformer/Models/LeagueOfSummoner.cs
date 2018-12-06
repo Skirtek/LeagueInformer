dc694ca6eb846d5224daf95bff32926dd2771aa6
LAPTOP-IMPS8VN3\drazk <drazkowski1999@gmail.com>
2018-12-06 12:18:56 +0100

    LI-5 stworzenie serwisu do pobierania  potrzebnych informacji o lidze oraz dodanie modeli

---
+++ LeagueInformer/LeagueInformer/Models/LeagueOfSummoner.cs
@@ -0,0 +1,21 @@
+﻿namespace LeagueInformer.Models
+{
+    public class Summoner
+    {
+
+        /// <summary>
+        /// necessery data for LeagueOfSummoner
+        /// </summary>
+        public string summonerName { get; set; }
+
+        public string tier { get; set; }
+
+        public string wins { get; set; }
+
+        public string losses { get; set; }
+
+        public string leagueName { get; set; }
+
+        public string queueType { get; set; }
+    }
+}
---
+++ LeagueInformer/LeagueInformer/Services/GetLeagueOfSummonerService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+using LeagueInformer.Api;
+using LeagueInformer.Enums;
+using LeagueInformer.Interfaces;
+using LeagueInformer.Models;
+using Newtonsoft.Json.Linq;
+
+namespace LeagueInformer.Services
+{
+    public class GetSummonerService : IGetSummoner
+    {
+        private readonly ApiClient _apiClient = new ApiClient();
+
+        public async Task<Summoner> GetInformationAboutSummoner(string id)
+        {
+            try
+            {
+                JObject response = JObject.Parse(await _apiClient.GetJsonFromUrl(
+                    $"https://eun1.api.riotgames.com/lol/league/v4/positions/by-summoner/{id}?api_key={AppSettings.AuthorizationApiKey}"));
+
+                return response == null ? new Summoner { IsSuccess = false } :
+                    new Summoner
+                    {
+                        IsSuccess = true,
+                        summonerName = response.GetValue("summonerName").ToString(),
+                        tier = response.GetValue("tier").ToString(),
+                        wins = response.GetValue("wins").ToString(),
+                        losses = response.GetValue("losses").ToString(),
+                        leagueName = response.GetValue("leagueName").ToString(),
+                        queueType = response.GetValue("queueType").ToString()
+                    };
+            }
+            catch (Exception ex)
+            {
+                return new Summoner { IsSuccess = false, Message = Error_Handler(ex.Message) };
+            }
+        }
+
+        private string Error_Handler(string message)
+        {
+            switch (message)
+            {
+                case string error when error.Contains("404"):
+                    return _apiClient.MapErrorToString(ErrorEnum.NotFound);
+                case string error when error.Contains("422"):
+                    return _apiClient.MapErrorToString(ErrorEnum.PlayerHasNotMatchHistory);
+                case string error when error.Contains("504"):
+                    return _apiClient.MapErrorToString(ErrorEnum.RequestTimeout);
+                case string error when error.Contains("500") || error.Contains("502") || error.Contains("503"):
+                    return _apiClient.MapErrorToString(ErrorEnum.InternalServerError);
+                case string error when error.Contains("400") || error.Contains("401")
+                                                             || error.Contains("403") || error.Contains("404")
+                                                             || error.Contains("415") || error.Contains("429"):
+                    return _apiClient.MapErrorToString(ErrorEnum.RequestAppError);
+                default:
+                    return _apiClient.MapErrorToString(ErrorEnum.DownloadingError);
+            }
+        }
+    }
+}
